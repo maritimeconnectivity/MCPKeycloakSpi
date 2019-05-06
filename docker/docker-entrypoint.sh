79fbc394394d911a2eeaa5efd509b8504346f1bc
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2019-05-06 14:19:19 +0200

    enabled script based authenticators

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -85,5 +85,5 @@ if [ "$DB_VENDOR" != "h2" ]; then
     /bin/sh /opt/jboss/tools/databases/change-database.sh $DB_VENDOR
 fi
 
-exec /opt/jboss/keycloak/bin/standalone.sh -Djboss.node.name=$HOSTNAME -Djgroups.bind_addr=global -b $HOSTNAME $@
+exec /opt/jboss/keycloak/bin/standalone.sh -Djboss.node.name=$HOSTNAME -Djgroups.bind_addr=global -b $HOSTNAME $@ -Dkeycloak.profile.feature.scripts=enabled
 exit $?
